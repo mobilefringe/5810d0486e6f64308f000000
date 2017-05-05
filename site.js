8b0cc086fca095e3842124b1177cb14e897d406d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-05-05 10:25:56 -0400

    Rio Washingtonian 1.1  /site.js

---
+++ site.js
@@ -0,0 +1,7 @@
+var site_json = {
+                    "mall_name" : "RIO Washingtonian Center",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/5810d0486e6f64308f000000/image/png/1479160246000/logo copy.png",
+                    "time_zone" : "US/Eastern",
+                    "newsletter_form_url" : "//mobilefringe.createsend.com/t/d/s/bitjtd/?callback=?",
+                    "newsletter_email_field" : "cm-bitjtd-bitjtd"
+                }
