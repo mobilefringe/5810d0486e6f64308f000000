1171f6df5e2db274660883e18daaaeb515e01bdb
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-10-12 14:25:57 -0400

    Rio Washingtonian 1.3  /application.js

--- application.js
+++ application.js
@@ -73,7 +73,9 @@ function renderPromotions(container, template, collection){
             val.store_detail_btn = store_details.slug ;
             val.store_name = store_details.name;
             val.image_url = store_details.store_front_url_abs;
-            val.cat_list = store_details.categories.join(',');
+            if(store_details.categories != null){
+                val.cat_list = store_details.categories.join(',');
+            }
         }
         else{
             val.store_name = site_json.mall_name;
