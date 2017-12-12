48df15e573eb9de7527237f95651e5e959fa59a6
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-12 11:43:39 -0500

    Rio Washingtonian 1.3  /helpers.js

--- helpers.js
+++ helpers.js
@@ -48,8 +48,8 @@ function init(e){
     })
     
     //dynamically changing copyright year
-var current_year = moment().year();
-$("#current_year").text(current_year);
+    var current_year = moment().year();
+    $("#current_year").text(current_year);
 
 }
 
